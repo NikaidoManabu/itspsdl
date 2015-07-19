69c101bfa2f4f46d21974f41c3cb941c5a9458e7
nikaidomanabu(ITSPmac) <TS@nikaidomanabu-no-MacBook-Air.local>
2015-07-20 00:11:03 +0900

    プロジェクト移動・webview/header/footer作成/角度取得まで

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/degscrollwebview/MainActivity.java
@@ -0,0 +1,101 @@
+package jp.ac.titech.itpro.sdl.degscrollwebview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class MainActivity extends Activity  implements SensorEventListener{
+    private SensorManager manager;
+    private TextView values;
+    private TextView values2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //レイアウトで指定したWebViewのIDを指定する。
+        WebView  myWebView = (WebView)findViewById(R.id.web_view);
+
+        //リンクをタップしたときに標準ブラウザを起動させない
+        myWebView.setWebViewClient(new WebViewClient());
+
+        //最初にYahoo! Japanのページを表示する。
+        myWebView.loadUrl("http://www.yahoo.co.jp/");
+        myWebView.getSettings().setBuiltInZoomControls(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        values = (TextView)findViewById(R.id.header_text);
+        values2 = (TextView)findViewById(R.id.header_text2);
+        manager = (SensorManager)getSystemService(SENSOR_SERVICE);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Listenerの登録解除
+        manager.unregisterListener(this);
+     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Listenerの登録
+        List<Sensor> sensors = manager.getSensorList(Sensor.TYPE_ORIENTATION);
+        if(sensors.size() > 0) {
+            Sensor s = sensors.get(0);
+            manager.registerListener(this, s, SensorManager.SENSOR_DELAY_UI);
+        }
+     }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // TODO Auto-generated method stub
+        if(event.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+            String str = "傾きセンサー値:"
+                        + "\n方位角:" + event.values[0]
+                        + "\n傾斜角:" + event.values[1]
+                        + "\n回転角:" + event.values[2];
+        values.setText(str);
+        }
+    }
+
+}
